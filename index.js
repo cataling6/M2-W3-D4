8919b011943a70f1dd39b84849375c77bb82ae72
Catalin Radoi <radoi.catalin88@gmail.com>
2023-10-27 18:58:28 +0200

    up risorse

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-function changeH1 (){
-	
-	document.querySelector("h1").innerText ="ahahah ma sei bravo";
+function changeH1() {
+  document.querySelector("h1").innerText = "ahahah ma sei bravo";
 }
 
-function changeBg () {
-	document.body.style.background ="black";
-	document.body.style.color="white";
+function changeBg() {
+  document.body.style.background = "black";
+  document.body.style.color = "white";
 }
 
-function changeAddress (){
-	let modifiedFooter = document.querySelector("footer").innerText;
-	
-	modifiedFooter = modifiedFooter.split("-");
-	modifiedFooter[1] = "via sei un grande nr 29, 00000 Roma(nia), Italy";
-	document.querySelector("footer").innerText = modifiedFooter.toString().replace(',', "- ");
+function changeAddress() {
+  let modifiedFooter = document.querySelector("footer").innerText;
+
+  modifiedFooter = modifiedFooter.split("-");
+  modifiedFooter[1] = "via sei un grande nr 29, 00000 Roma(nia), Italy";
+  document.querySelector("footer").innerText = modifiedFooter
+    .toString()
+    .replace(",", "- ");
 }
 
-function changeClassAmazon (){
-	document.querySelectorAll("a").style.
+function changeClassAmazon() {
+  //document.querySelectorAll("a").style.
 }
 
-
-changeH1 ();
+changeH1();
 changeBg();
 changeAddress();
